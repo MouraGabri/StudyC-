3d3708b02ffcd454b70d9443bd0f459616914e2b
MouraGabri <mouragabri351@yahoo.com>
2025-05-20 22:54:59 -0300

    Praticando Exercicio URI

---
+++ estrutura_condicional/ex_1046_uri.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.estrutura_condicional
+{
+    internal class ex_1046_uri
+    {
+        public static void TempoJogo()
+        {
+            string[] entrada = Console.ReadLine().Split();
+            int horaInicio = int.Parse(entrada[0]);
+            int horaFinal = int.Parse(entrada[1]);
+            int duracao = 0;
+
+            if (horaInicio >= horaFinal)
+            {
+                duracao = (24 + horaFinal) - horaInicio;
+            }
+            else
+            {
+                duracao =  horaFinal - horaInicio;
+
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+
+        }
+    }
+}

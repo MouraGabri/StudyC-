6696a423ec4c3eaf8da7e02eba63399c3890157e
MouraGabri <mouragabri351@yahoo.com>
2025-05-26 22:36:42 -0300

    Praticando Exercicio URI

---
+++ estrutura_condicional/ex_1045_uri.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.estrutura_condicional
+{
+    internal class ex_1045_uri
+    {
+        public static void TiposDeTriangulo()
+        {
+
+            string[] entrada = Console.ReadLine().Split(' ');
+            double a = double.Parse(entrada[0]);
+            double b = double.Parse(entrada[1]);
+            double c = double.Parse(entrada[2]);
+
+
+            if (Math.Pow(a, 2) > (Math.Pow(b, 2) + Math.Pow(c, 2)))
+            {
+                Console.WriteLine("TRIANGULO OBTUSANGULO");
+            }
+
+            if (Math.Pow(a, 2) == Math.Pow(b, 2) + Math.Pow(c, 2))
+            {
+                Console.WriteLine("TRIANGULO RETANGULO");
+
+            }
+
+            if (Math.Pow(a, 2) < (Math.Pow(b, 2) + Math.Pow(c, 2)))
+            {
+                Console.WriteLine("TRIANGULO ACUTANGULO");
+
+            }
+            if (a >= b + c)
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+
+            }
+            if (a == b && a == c)
+            {
+                Console.WriteLine("TRIANGULO EQUILATERO");
+
+            }
+
+            if (a == b | b == c | c == a)
+            {
+                Console.WriteLine("TRIANGULO ISOSCELES");
+
+            }
+
+
+        }
+
+    }
+}
---
+++ estrutura_condicional/ex_1047_uri.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.estrutura_condicional
+{
+    internal class ex_1047_uri
+    {
+        public static void TempoDeJogoMinutos()
+        {
+            //inicial,minuto final,hora final,minuto final - jogo
+            string[] entrada = Console.ReadLine().Split();
+            int horaInicial = int.Parse(entrada[0]);
+            int minutoInicial = int.Parse(entrada[1]);
+            int horaFinal = int.Parse(entrada[2]);
+            int minutoFinal = int.Parse(entrada[3]);
+
+            int segundosInicio = (horaInicial * 3600) + (minutoInicial * 60);
+            int segundosFinal = (horaFinal * 3600) + (minutoFinal * 60);
+            int duracaoTotalSegundos;
+
+            if (segundosFinal > segundosInicio)
+            {
+                duracaoTotalSegundos = segundosFinal - segundosInicio;
+            }
+
+            else
+            {
+                duracaoTotalSegundos = (86400 - segundosInicio) + segundosFinal;
+
+            }
+
+            int duracaoHoras = duracaoTotalSegundos / 3600;
+            duracaoTotalSegundos %= 3600;
+
+            int duraMinutos = duracaoTotalSegundos / 60;
+
+
+            Console.WriteLine($"O JOGO DUROU {duracaoHoras} HORA(S) E {duraMinutos} MINUTO(S)");
+
+        }
+
+
+
+
+
+    }
+}

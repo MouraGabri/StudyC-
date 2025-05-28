ad4cdf81ea008e95fdec4f894c16ade7e3bc823f
MouraGabri <mouragabriel351@yahoo.com>
2025-05-27 23:31:23 -0300

    Tentando fazer um cálc de IR

---
+++ estrutura_condicional/ex_1051_uri.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.estrutura_condicional
+{
+    internal class ex_1051_uri
+    {
+
+        public static void ImpostoDeRenda()
+        {
+            double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double valor1 ;
+            double valor2 ;
+
+
+            if (renda >= 0.00 && renda <= 2000.00)
+            {
+                Console.WriteLine("Isento");
+            }
+
+            else if (renda > 2000.00 && renda <= 3000.00)
+            {
+                //valor = (double)3000.00 - renda;
+                //valor = valor * 0.08;
+            }
+
+            else if (renda > 3000.00 && renda <= 4500.00)
+            {
+                
+                valor1 = 1000 * 0.18;
+            }
+
+            else
+            {
+
+            }
+        }
+    }
+}

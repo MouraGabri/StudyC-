cfe94c0b5f6b8fad7c501016f0a3648e65d412e2
MouraGabri <mouragabriel351@yahoo.com>
2025-06-01 20:03:31 -0300

    Começando o módulo de estruturas de repetição

---
+++ estrutura_repeticao/learning_while.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.estrutura_repeticao
+{
+    internal class learning_while
+    {
+
+        public static void While()
+        {
+            int x = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            while (x != 0)
+            {
+                soma += x;
+                x = int.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine(soma);
+            Console.WriteLine();
+        }
+    }
+}

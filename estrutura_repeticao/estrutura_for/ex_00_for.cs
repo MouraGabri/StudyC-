277970b65496ae837b71c6423e3cbfb6df7fce70
MouraGabri <mouragabriel351@yahoo.com>
2025-06-18 21:41:26 -0300

    Iniciando os estudos com a estrutura for em C#

---
+++ estrutura_repeticao/estrutura_for/ex_00_for.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace estudosC_.estrutura_repeticao.estrutura_for
+{
+    internal class ex_00_for
+    {
+        public static void AprendizadoFor()
+        {
+            int n, x, soma = 0;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                soma += x;
+            }
+            Console.WriteLine(soma);
+
+
+
+        }
+    }
+}

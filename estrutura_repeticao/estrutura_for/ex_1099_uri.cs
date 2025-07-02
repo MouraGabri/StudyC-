d9b2a8a5ff00db0f656f0e46b4895da64a99ca7b
MouraGabri <mouragabriel351@yahoo.com>
2025-07-01 22:08:33 -0300

    Exercicios com for em C#

---
+++ estrutura_repeticao/estrutura_for/ex_1099_uri.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.estrutura_repeticao.estrutura_for
+{
+    internal class ex_1099_uri
+    {
+        public static void SomaImparesConsecutivos()
+        {
+            Console.Write("Informe a quantidade de casos:");
+            int qtdRepeticoesLinhas = int.Parse(Console.ReadLine());
+            int maior, menor;
+
+
+            for (int i = 0; i < qtdRepeticoesLinhas; i++)
+            {
+                Console.Write("Valor de X:");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("Valor de Y:");
+                int y = int.Parse(Console.ReadLine());
+                int j;
+
+                if (x > y) { maior = x; menor = y; }
+                else { maior = y; menor = x; }
+                int soma = 0;
+
+                for (j = menor + 1; j < maior; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        soma += j;
+                    }
+                }
+                    Console.WriteLine(soma);
+                
+
+
+
+
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ estrutura_repeticao/estrutura_for/ex_1116_uri.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.estrutura_repeticao.estrutura_for
+{
+    internal class ex_1116_uri
+    {
+        public static void DividindoXPorY()
+        {
+            Console.Write("Informe a quantidade de pares:");
+            int qtdRepeticoesLinhas = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < qtdRepeticoesLinhas; i++)
+            {
+                Console.Write("Valor de X:");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("Valor de Y:");
+                int y = int.Parse(Console.ReadLine());
+
+                double resposta = 0.0 ;
+
+
+                if(y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+
+                }
+                else
+                {
+                    resposta = (double)x / y;
+                    Console.WriteLine(resposta.ToString("F1", CultureInfo.InvariantCulture));
+                }
+
+                
+                
+            }
+
+
+
+        }
+    }
+}

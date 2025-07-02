7c209afbcedf9a4b35ace2f41a988e54bd70629f
MouraGabri <mouragabriel351@yahoo.com>
2025-07-01 23:20:54 -0300

    Exercicios C#

---
+++ estrutura_repeticao/estrutura_for/ex_1101_uri.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.estrutura_repeticao.estrutura_for
+{
+    internal class ex_1101_uri
+    {
+        public static void SequenciaNumerosESoma()
+        {
+
+            int x = 1;
+            int y = 1;
+            int maior, menor;
+
+            while (x > 0 && y > 0)
+            {
+
+                Console.Write("Valor de X:");
+                x = int.Parse(Console.ReadLine());
+
+                Console.Write("Valor de Y:");
+                y = int.Parse(Console.ReadLine());
+
+                if (x > y) { maior = x; menor = y; }
+                else { maior = y; menor = x; }
+                int soma = 0;
+                for (int i = menor; i <= maior; i++)
+                {
+                    Console.Write(i + ",");
+                    soma += i;
+
+                    if (i == maior)
+                    {
+                        Console.WriteLine($"Sum=  {soma} ");
+                    }
+                }
+            }
+        }
+
+    }
+}

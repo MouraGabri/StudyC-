9d6d394dc371daeafb55f96ab7301c0bfbdd1ca3
MouraGabri <mouragabriel351@yahoo.com>
2025-07-02 23:14:36 -0300

    Exercicio 1158 funcionando, porém preciso simplificar a lógica

---
+++ estrutura_repeticao/estrutura_for/ex_1158_uri.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.estrutura_repeticao.estrutura_for
+{
+    internal class ex_1145_uri
+    {
+        public static void SequenciaLogica2()
+        {
+            Console.Write("N(Qtd de casos de teste):");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("X(Número incial da sequência):");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Valor de Y(Qtd de números impares que deve somar):");
+            int y = int.Parse(Console.ReadLine());
+            int inicio = x;
+           
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (x % 2 == 0)
+                {
+                    inicio = x + 1;
+                }
+                else
+                {
+                    inicio = x;
+                }
+                int soma = 0;
+                for (int j = 0;  j < y; j++)
+                {
+             
+                     Console.WriteLine(inicio);
+            
+                    soma += inicio;
+                    inicio += 2;
+                    //Console.WriteLine(soma);
+                }
+
+                Console.Write("X(Número incial da sequência):");
+                x = int.Parse(Console.ReadLine());
+
+                Console.Write("Valor de Y(Qtd de números impares que deve somar):");
+                y = int.Parse(Console.ReadLine());
+
+            }
+            
+
+        }
+    }
+}

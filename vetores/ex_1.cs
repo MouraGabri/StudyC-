db65e72d706b405c9a136b3b6bb8dbf2e6f0c4aa
MouraGabri <mouragabriel351@yahoo.com>
2025-07-05 22:56:25 -0300

    Exercicios com vetores

---
+++ vetores/ex_1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace estudosC_.vetores
+{
+    internal class ex_1
+    {
+        public static void NumeroMenor()
+        {
+            ;
+            Console.Write("QTD de valores:");
+            int N = int.Parse(Console.ReadLine());
+            int[] vet = new int[N];
+            int numeroMaior = vet[0];
+
+            //Armazenando no vetor os valores
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Informe um valor:");
+                int valores = int.Parse(Console.ReadLine());
+                vet[i] = valores;
+            }
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                if (numeroMaior > vet[i + 1])
+                {
+                    Console.WriteLine("Número maior" + numeroMaior);
+                    int numberMaior = numeroMaior;
+                }
+                else
+                {
+                    numeroMaior = vet[i + 1];
+                }
+            }
+
+
+        }
+
+    }
+}

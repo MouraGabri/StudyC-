851a02e62b422099bd3c4229cf7a1a1063fc202d
MouraGabri <mouragabriel351@yahoo.com>
2025-07-06 20:01:05 -0300

    Praticando exercicios de vetores

---
+++ vetores/ex_3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.vetores
+{
+    internal class ex_3
+    {
+        public static void NumerosPares()
+        {
+            Console.Write("QTD de valores:");
+            int N = int.Parse(Console.ReadLine());
+            int[] vet = new int[N];
+            int contadorNumerosPares = 0;
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write("Informe um valor:");
+                int valor = int.Parse(Console.ReadLine());
+                vet[i] = valor;
+            }
+
+            for ( int i = 0; i < vet.Length; i ++)
+            {
+                if (vet[i] % 2 == 0) { 
+                Console.Write(vet[i]+",");
+                contadorNumerosPares++;
+
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine(contadorNumerosPares);
+
+
+
+
+
+
+
+        }
+    }
+}

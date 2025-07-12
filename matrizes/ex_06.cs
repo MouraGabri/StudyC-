f78a43ec4011f728d4087298b59d03fd4f53251d
MouraGabri <mouragabriel351@yahoo.com>
2025-07-12 19:42:22 -0300

    Módulo de Matriz Encerrado

---
+++ matrizes/ex_06.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.matrizes
+{
+    internal class ex_06
+    {
+        public static void NovaMatriz()
+        {
+            Console.Write("Linhas Matriz:");
+            int linhas = int.Parse(Console.ReadLine());
+            Console.Write("Colunas Matriz:");
+            int colunas = int.Parse(Console.ReadLine());
+            int[,] A = new int[linhas, colunas];
+            int[,] B = new int[linhas, colunas];
+            int[,] C = new int[linhas, colunas];
+
+
+            Console.WriteLine("Inserindo valores na matriz A:");
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    int valor = int.Parse(Console.ReadLine());
+                    A[i, j] = valor;
+                }
+            }
+            Console.WriteLine("=============================================");
+
+            Console.WriteLine("Inserindo valores na matriz B:");
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    int valor = int.Parse(Console.ReadLine());
+                    B[i, j] = valor;
+                }
+            }
+            Console.WriteLine("=============================================");
+
+            Console.WriteLine("Imprimindo a matriz C:");
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    C[i, j] = A[i, j] + B[i, j];
+                    Console.Write(C[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("=============================================");
+        }
+
+
+    }
+}

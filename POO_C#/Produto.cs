367f07390bc325a222c3ba3788a1e78632400742
MouraGabri <mouragabriel351@yahoo.com>
2025-07-22 18:29:53 -0300

    Começando os estudos em POO

---
+++ POO_C#/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.POO_C_
+{
+    internal class Produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidade;
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return preco * quantidade;
+        }
+
+        public static void AdicionarProdutos(int quantidade)
+        {
+
+        }
+
+        public static void RemoverProdutos(int quantidade)
+        {
+
+        }
+
+    }
+
+
+
+}
+
---
+++ POO_C#/TestarProduto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.POO_C_
+{
+    internal class TestarProduto
+    {
+
+        public static void ExecutarProduto()
+        {
+            Produto pdt;
+            pdt = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome:");
+            pdt.nome = Console.ReadLine();
+            Console.Write("Preço:");
+            pdt.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no Estoque:");
+            pdt.quantidade = int.Parse(Console.ReadLine());
+        }
+
+
+    }
+}

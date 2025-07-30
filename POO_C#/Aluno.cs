b10229743393e4f6ae6ba632e97adc77c0f793a7
MouraGabri <mouragabriel351@yahoo.com>
2025-07-29 23:39:03 -0300

    Começando os estudos em POO

---
+++ POO_C#/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosC_.POO_C_
+{
+    internal class Aluno
+    {
+
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+
+        public void AlunoAprovado()
+        {
+            double notaAprovado = 60.00;
+            double somaNotas = Nota1 + Nota2 + Nota3;
+            if (somaNotas >= notaAprovado)
+            {
+                Console.WriteLine($"Nota Final: {somaNotas.ToString("F2", CultureInfo.InvariantCulture)}\nAprovado");
+            }
+            else
+            {
+                double pontosFaltam = notaAprovado - somaNotas;
+                Console.WriteLine($"Nota Final: {somaNotas.ToString("F2", CultureInfo.InvariantCulture)}\nReprovado\nFaltaram {pontosFaltam.ToString("F2", CultureInfo.InvariantCulture)} Pontos");
+
+            }
+        }
+
+    }
+}
---
+++ POO_C#/TestarAluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace estudosC_.POO_C_
+{
+    internal class TestarAluno
+    {
+
+        public static void ExecutarAluno()
+        {
+            Aluno aluno = new Aluno();
+            Console.Write("Nome do aluno:");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as 3 notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.AlunoAprovado();
+        }
+    }
+}
